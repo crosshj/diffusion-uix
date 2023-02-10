a65e17278deaaf6ba2d2b9b8528a5a52a5a55473
crosshj <github@chimpjuice.com>
2023-02-09 19:20:35 -0500

    state is external

--- src/alt-ux/index.js
+++ src/alt-ux/index.js
@@ -1,2 +1,62 @@
-import theme from './theme.js';
-window.theme = theme;
+import getTheme from './theme.js';
+window.getTheme = getTheme;
+
+
+class PubSubState {
+	listeners = {};
+	state = {};
+
+	constructor({ initialState } = {}){
+		this.state = initialState || this.state;
+		this.get = this.get.bind(this);
+		this.remove = this.remove.bind(this);
+		this.listen = this.listen.bind(this);
+		this.trigger = this.trigger.bind(this);
+	}
+	get(key){
+		return this.state[key] !== undefined
+			? this.state[key]
+			: this.state;
+	}
+	remove(key, hash){
+		this.listeners[key] = this.listeners[key]
+			.filter(x => x.hash !== hash);
+	};
+	listen(key, fn, priority=0) {
+		this.listeners[key] = this.listeners[key] || [];
+		const hash = [...Array(30)].map(() => Math.random().toString(36)[2]).join('');
+		if (priority){
+			this.listeners[key].unshift({ fn, hash });
+		} else {
+			this.listeners[key].push({ fn, hash });
+		}
+		return { remove: () => this.remove(key, hash) };
+	}
+	trigger(key, arg) {
+		if (!this.listeners[key]) {
+			console.log(' listener not registered');
+			console.log({ listeners: this.listeners, key });
+			return;
+		}
+		const prev = this.state;
+		const next = typeof arg === "function"
+			? fn(prev)
+			: { ...prev, [key]: arg };
+		this.listeners[key]
+			.forEach((x) => x.fn(next[key]) );
+	}
+}
+
+const initialState = {
+	checkpoint: 'loading',
+	currentTab: 0,
+	vae: 'loading',
+	theme: getTheme()
+};
+window.state = new PubSubState({ initialState });
+
+//NOTE: simulate loading from network
+setTimeout(() => {
+	state.trigger("checkpoint", 30);
+	state.trigger("vae", 30);
+}, 2000);
--- src/alt-ux/react.jsx
+++ src/alt-ux/react.jsx
@@ -1,3 +1,5 @@
+const { useState } = React;
+
 const {
 	CssBaseline,
 	ThemeProvider,
@@ -5,8 +7,7 @@ const {
 	Container,
 	Box,
 	SvgIcon,
-	Select,
-	MenuItem,
+	NativeSelect: Select,
 	FormControl,
 	InputLabel,
 	Paper,
@@ -26,58 +27,71 @@ const {
 	eg. <Icon sx={{ fontSize: 30 }}>add_circle</Icon>
 */
 
+const SelectList = ({ stateKey, label, children }) => {
+	const [value, setter] = useState(state.get(stateKey));
+	state.listen(stateKey, setter);
+	const onChange = (e) => state.trigger(stateKey, e.target.value);
 
-const Header = ({ state, trigger }) => {
-	const { checkpoint, vae } = state;
-	const handleChange = (e, key) => {
-		trigger({ key, value: e.target.value });
-	};
+	return (
+		<FormControl variant="standard" sx={{ m: 1, minWidth: 300 }}>
+			<InputLabel htmlFor="checkpoint-select">{label}</InputLabel>
+			{ value === "loading" &&
+				<Select
+					id={stateKey + '-selector'}
+					size="small"
+					label={label}
+					value="loading"
+					disabled
+					onChange={onChange}
+				>
+					<option value="loading">Loading...</option>
+				</Select>
+			}
+			{ value !== "loading" &&
+				<Select
+					id={stateKey + '-selector'}
+					size="small"
+					label={label}
+					value={value}
+					onChange={onChange}
+				>
+					{ children }
+				</Select>
+			}
+		</FormControl>
+	);
+};
+
+const Header = () => {
 	return (
 		<div style={{ margin: "0.4rem", marginBottom: 0 }}>
-			<FormControl variant="standard" sx={{ m: 1, minWidth: 300 }}>
-				<InputLabel id="checkpoint-label">Checkpoint</InputLabel>
-				<Select
-					size="small"
-					labelId="checkpoint-label"
-					label="Checkpoint"
-					value={checkpoint}
-					onChange={(e) => handleChange(e, 'checkpoint')}
-				>
-					<MenuItem value={10}>pfg_111.ckpt [5a369d04a0]</MenuItem>
-					<MenuItem value={20}>pokemonStyle_v1.ckpt [4e84fa37d8]</MenuItem>
-					<MenuItem value={30}>protogenV22AnimeOffi_22.safetensors [1254103966]</MenuItem>
-				</Select>
-			</FormControl>
-			<FormControl variant="standard" sx={{ m: 1, minWidth: 300 }}>
-				<InputLabel id="vae-label">VAE</InputLabel>
-				<Select
-					size="small"
-					labelId="vae-label"
-					label="VAE"
-					value={vae}
-					onChange={(e) => handleChange(e, 'vae')}
-				>
-					<MenuItem value={10}>None</MenuItem>
-					<MenuItem value={20}>Automatic</MenuItem>
-					<MenuItem value={30}>vae-ft-mse-840000-ema-pruned.ckpt</MenuItem>
-				</Select>
-			</FormControl>
+			<SelectList stateKey="checkpoint" label="Checkpoint">
+				<option value={10}>pfg_111.ckpt [5a369d04a0]</option>
+				<option value={20}>pokemonStyle_v1.ckpt [4e84fa37d8]</option>
+				<option value={30}>protogenV22AnimeOffi_22.safetensors [1254103966]</option>
+			</SelectList>
+			<SelectList stateKey="vae" label="VAE">
+				<option value={10}>None</option>
+				<option value={20}>Automatic</option>
+				<option value={30}>vae-ft-mse-840000-ema-pruned.ckpt</option>
+			</SelectList>
 		</div>
 	);
 };
 
-const Body = ({ state, trigger }) => {
-	const { currentTab } = state;
-	const handleChange = (value, key) => {
-		trigger({ key, value });
-	};
+const Body = () => {
+	const [currentTab, setCurrentTab] = useState(state.get('currentTab'));
+	state.listen('currentTab', setCurrentTab);
+
+	const onChange = (key) => (e, newValue) => state.trigger(key, newValue);
+
 	return (
 		<div  style={{ margin: "0 auto", marginBottom: "auto" }}>
 			<Box sx={{ bgcolor: 'background.paper' }} >
 				<Box sx={{ width: "98vw", borderBottom: 1, borderColor: 'divider', bgcolor: 'background.paper' }}>
 					<Tabs
 						value={currentTab}
-						onChange={(e, newValue) => handleChange(newValue, 'currentTab')}
+						onChange={onChange('currentTab')}
 						variant="scrollable"
 						scrollButtons="auto"
 					>
@@ -110,7 +124,6 @@ const Body = ({ state, trigger }) => {
 };
 
 const Footer = () => {
-	//console.log('-- render footer');
 	return (
 		<div id="footer">
 			<div>API  •  Github  •  Gradio  •  Reload UI</div>
@@ -137,18 +150,19 @@ const LayoutContainer = styled('div')(({ theme }) => ({
 	display: "flex",
 	flexDirection: "column"
 }));
+const App = () => {
+	const [theme, setTheme] = useState(state.get('theme'));
+	state.listen('theme', setTheme);
 
-const App = ({ state, trigger }) => {
-	//console.log('-- render layout');
 	return (
-		<ThemeProvider theme={theme()}>
+		<ThemeProvider theme={theme}>
 			<CssBaseline />
 			<LayoutContainer>
 				<LayoutItem height="auto">
-					<Header state={state} trigger={trigger}/>
+					<Header />
 				</LayoutItem>
 				<LayoutItem>
-					<Body state={state} trigger={trigger} />
+					<Body />
 				</LayoutItem>
 				<LayoutItem height="auto">
 					<Footer />
@@ -159,42 +173,10 @@ const App = ({ state, trigger }) => {
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-const exampleState = {
-	checkpoint: 10,
-	currentTab: 0,
-	vae: 30,
-};
-
-class ReactState {
-	constructor({ render }){
-		this.state = exampleState;
-		this.render = () => render(this);
-		this.get = this.getAll.bind(this);
-		this.trigger = this.trigger.bind(this);
-	}
-	// publish(trigger) - a state change is trigger somewhere in react, all functions subscribed to this state are called + component updates
-	// subscribe(bind) - component adds its function as a subscription to some part of state (like useState)
-
-	getAll(){
-		return this.state;
-	}
-
-	trigger(action={}){
-		const { key, value='' } = action;
-		if(key){
-			this.state[key] = value;
-		}
-		this.render();
-		return;
-	}
-}
-window.state = new ReactState({
-	render: (s) => root.render(<App state={s.get()} trigger={s.trigger} />)
-});
-
-state.trigger();
+root.render(<App />);
 
 window
 	.matchMedia("(prefers-color-scheme: dark)")
-	.addEventListener("change", state.trigger);
+	.addEventListener("change", () => {
+		state.trigger("theme", getTheme())
+	});
